ddc1c96dc95e46b3a8663e3b77bdcc51d7e7d2cc
pranav-kural <pranavkural042@gmail.com>
2024-07-13 20:21:04 -0400

    added methods to infer data type from file extension #31

--- src/rag/data-loaders/data-loaders.ts
+++ src/rag/data-loaders/data-loaders.ts
@@ -10,6 +10,32 @@ import {Document} from 'langchain/document';
 export type SupportedDataLoaderTypes = 'text' | 'json' | 'csv' | 'code' | 'pdf';
 
 /**
+ * Supported file extensions for data loaders.
+ */
+export const supportedFileExtensions: {[key: string]: string[]} = {
+  cpp: ['cpp', 'cxx', 'cc'],
+  go: ['go'],
+  java: ['java'],
+  js: ['js', 'mjs', 'cjs'],
+  php: ['php'],
+  proto: ['proto'],
+  python: ['py'],
+  rst: ['rst'],
+  ruby: ['rb'],
+  rust: ['rs'],
+  scala: ['scala'],
+  swift: ['swift'],
+  markdown: ['md', 'markdown', 'mdx'],
+  latex: ['tex', 'ltx'],
+  html: ['html', 'htm'],
+  sol: ['sol'],
+  text: ['txt'],
+  json: ['json'],
+  pdf: ['pdf'],
+  csv: ['csv'],
+};
+
+/**
  * Keys to include when loading JSON data.
  */
 export type JSONLoaderKeysToInclude = string | string[];
@@ -38,6 +64,51 @@ export type PDFLoaderOptions = {
 };
 
 /**
+ * Validate the data type of the file based on the file extension.
+ * @param filePath path to the data file from which the file type is inferred using the file extension
+ * @returns is supported type: dataType and isSupported as true, else unsupported type: unSupportedDataType and isSupported as false
+ */
+export function validateDataType(filePath: string):
+  | {
+      dataType: SupportedDataLoaderTypes;
+      isSupported: true;
+    }
+  | {
+      unSupportedDataType: string;
+      isSupported: false;
+    } {
+  // Validate the file path and extract the file extension
+  const extension = filePath.split('.').pop();
+  if (!extension) {
+    return {unSupportedDataType: '', isSupported: false};
+  }
+
+  // Normalize the extracted file extension to lowercase
+  const fileExtension = extension.toLowerCase();
+
+  // Check if the extracted file extension is in any of the supported extensions lists
+  if (fileExtension) {
+    for (const [fileType, extensions] of Object.entries(
+      supportedFileExtensions
+    )) {
+      if (extensions.includes(fileExtension)) {
+        if (
+          fileType === 'text' ||
+          fileType === 'json' ||
+          fileType === 'pdf' ||
+          fileType === 'csv'
+        ) {
+          return {dataType: fileType, isSupported: true};
+        } else {
+          return {dataType: 'code', isSupported: true};
+        }
+      }
+    }
+  }
+  return {unSupportedDataType: fileExtension, isSupported: false};
+}
+
+/**
  * Get documents from a file path using a data loader.
  *
  * @param dataLoaderType Type of data being load. Choose from one of the supported data types.
@@ -61,6 +132,7 @@ export const getDocs = async (
   // infer loader to use based on dataLoaderType
   switch (dataLoaderType) {
     case 'text':
+    case 'code':
       loader = new TextLoader(path);
       break;
     case 'json':
