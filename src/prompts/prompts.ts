2efac281841128dc91dd14f01b6e1e9e9a3ce316
pranav-kural <pranavkural042@gmail.com>
2024-07-27 20:34:24 -0400

    Updated prompts with partials and custom output schema #43

--- src/prompts/prompts.ts
+++ src/prompts/prompts.ts
@@ -1,4 +1,19 @@
+import {JSONSchema} from '@genkit-ai/core/schema';
+import {z} from 'zod';
+
 // export prompts
+
+/**
+ * Type to define the output schema for a prompt.
+ */
+export type PromptOutputSchema = {
+  /** Desired output format. */
+  format?: 'text' | 'media' | 'json';
+  /** Zod schema defining the output structure (cannot be specified with non-json format). */
+  schema?: z.ZodTypeAny;
+  /** JSON schema of desired output (cannot be specified with non-json format). */
+  jsonSchema?: JSONSchema;
+};
 
 export {
   openEndedSystemPrompt,
