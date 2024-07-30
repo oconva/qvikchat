ef68093d26c8a9a7c3879291210d51de3cbc39ba
pranav-kural <pranavkural042@gmail.com>
2024-07-30 17:05:13 -0400

    fix for LLM model configurations type inference (Gemini, OpenAI, DALL-E) #43

--- src/models/models.ts
+++ src/models/models.ts
@@ -12,9 +12,8 @@ import {
   gpt4Vision,
   dallE3,
 } from 'genkitx-openai';
-import {DallE3ConfigSchema} from 'genkitx-openai/lib/dalle';
-import {OpenAiConfigSchema} from 'genkitx-openai/lib/gpt';
 import {z} from 'zod';
+import {GenerationCommonConfigSchema} from '@genkit-ai/ai/model';
 
 /**
  * List of supported Gemini models.
@@ -64,9 +63,10 @@ export const getSupportedModelNames = () => Object.values(SupportedModelNames);
 export type SupportedModels = keyof typeof SupportedModelNames;
 
 /**
- * Supported configuration options for a model
+ * Configuration options for Gemini models.
  */
-export type GeminiModelConfig = {
+type GeminiModelConfig = {
+  name: keyof typeof GeminiModels;
   version?: string | undefined;
   temperature?: number | undefined;
   maxOutputTokens?: number | undefined;
@@ -91,18 +91,95 @@ export type GeminiModelConfig = {
 };
 
 /**
+ * Configuration options for GPT models.
+ *
+ * May include the following properties:
+ * @property {name} - Name of the model.
+ * @property {frequencyPenalty} - Frequency penalty for sampling.
+ * @property {logitBias} - Logit bias for sampling.
+ * @property {logProbs} - Whether to return log probabilities.
+ * @property {presencePenalty} - Presence penalty for sampling.
+ * @property {seed} - Seed for sampling.
+ * @property {topLogProbs} - Top log probabilities to return.
+ * @property {user} - User for sampling.
+ * @property {visualDetailLevel} - Visual detail level for sampling.
+ */
+const OpenAiConfigSchema = GenerationCommonConfigSchema.extend({
+  frequencyPenalty: z.number().min(-2).max(2).optional(),
+  logitBias: z.record(z.string(), z.number().min(-100).max(100)).optional(),
+  logProbs: z.boolean().optional(),
+  presencePenalty: z.number().min(-2).max(2).optional(),
+  seed: z.number().int().optional(),
+  topLogProbs: z.number().int().min(0).max(20).optional(),
+  user: z.string().optional(),
+  visualDetailLevel: z.enum(['auto', 'low', 'high']).optional(),
+});
+
+/**
+ * Configuration options for DALL-E models.
+ */
+const DallE3ConfigSchema = GenerationCommonConfigSchema.extend({
+  size: z.enum(['1024x1024', '1792x1024', '1024x1792']).optional(),
+  style: z.enum(['vivid', 'natural']).optional(),
+  user: z.string().optional(),
+  quality: z.enum(['standard', 'hd']).optional(),
+  response_format: z.enum(['b64_json', 'url']).optional(),
+});
+
+/**
+ * Configuration options for OpenAI models.
+ *
+ * May include the following properties:
+ * @property {name} - Name of the model.
+ * @property {frequencyPenalty} - Frequency penalty for sampling.
+ * @property {logitBias} - Logit bias for sampling.
+ * @property {logProbs} - Whether to return log probabilities.
+ * @property {presencePenalty} - Presence penalty for sampling.
+ * @property {seed} - Seed for sampling.
+ * @property {topLogProbs} - Top log probabilities to return.
+ * @property {user} - User for sampling.
+ * @property {visualDetailLevel} - Visual detail level for sampling.
+ */
+type OpenAIModelConfig = {
+  name: keyof typeof OpenAIModels;
+} & z.infer<typeof OpenAiConfigSchema>;
+
+/**
+ * Configuration options for DALL-E models.
+ *
+ * May include the following properties:
+ * @property {name} - Name of the model.
+ * @property {size} - Size of the output image. One of '1024x1024', '1792x1024', or '1024x1792'.
+ * @property {style} - Style of the output image. Can be either 'vivid' or 'natural'.
+ * @property {quality} - Quality of the output image.
+ * @property {response_format} - Format of the response. Can be either 'b64_json' or 'url'.
+ */
+type DallEModelConfig = {
+  name: keyof typeof DallEModels;
+} & z.infer<typeof DallE3ConfigSchema>;
+
+/**
  * Configuration options for a model.
+ *
+ * May include the following properties:
+ *
+ *@prop {name} - Name of the model.
+ *@prop {version} - Version of the model.
+ *@prop {temperature} - Temperature for sampling.
+ *@prop {maxOutputTokens} - Maximum number of tokens to generate.
+ *@prop {topK} - Top K tokens to sample from.
+ *@prop {topP} - Top P tokens to sample from.
+ *@prop {stopSequences} - Sequences to stop generation at.
+ *@prop {safetySettings} - Safety settings for the model.
+ * @property {size} - Supported only by DALL-E models. Size of the output image. One of '1024x1024', '1792x1024', or '1024x1792'.
+ * @property {style} - Supported only by DALL-E models. Style of the output image. Can be either 'vivid' or 'natural'.
+ * @property {quality} - Supported only by DALL-E models. Quality of the output image.
+ * @property {response_format} - Supported only by DALL-E models. Format of the response. Can be either 'b64_json' or 'url'.
  */
 export type ModelConfig =
-  | ({
-      name: keyof typeof GeminiModels;
-    } & GeminiModelConfig)
-  | ({
-      name: keyof typeof OpenAIModels;
-    } & z.infer<typeof OpenAiConfigSchema>)
-  | ({
-      name: keyof typeof DallEModels;
-    } & z.infer<typeof DallE3ConfigSchema>);
+  | GeminiModelConfig
+  | OpenAIModelConfig
+  | DallEModelConfig;
 
 /**
  * Output schema for model responses.
